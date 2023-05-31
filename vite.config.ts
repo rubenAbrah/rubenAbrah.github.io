80d72a4bd8281cbe61fd6a5c45e602893b1e6dc4
Ruben <ruben.abrahamyan00@gmail.com>
2023-06-01 06:55:11 +0900

    --

--- vite.config.ts
+++ vite.config.ts
@@ -23,26 +23,22 @@ export default defineConfig({
         "description": "Vite PWA Boilerplate",
         "name": "Vite PWA Boilerplate",
         "icons": [
-            {
-                "src": "/icon-192x192.png",
-                "sizes": "192x192",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-256x256.png",
-                "sizes": "256x256",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-384x384.png",
-                "sizes": "384x384",
-                "type": "image/png"
-            },
-            {
-                "src": "/icon-512x512.png",
-                "sizes": "512x512",
-                "type": "image/png"
-            }
+          {
+            "src": "android-chrome-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "android-chrome-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any maskable"
+          }
         ]
       },
     }),
